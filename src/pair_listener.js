d875f31571629284ae2e51d3dd2b8b0b7d871846
M <129726658+mat1ab@users.noreply.github.com>
2023-05-23 14:08:51 -0500

    Update pair_listener.js

--- src/pair_listener.js
+++ src/pair_listener.js
@@ -6,18 +6,24 @@ let pair = new ethers.Contract(pairAddress, pairAbi, provider);
 let counter = 0;
 
 pair.on("Swap", function() {
-  counter++;
-  const timestamp = new Date().toISOString();
+  try {
+    counter++;
+    const timestamp = new Date().toISOString();
 
-  console.log(`Listening no.#${counter} swap event  at ${timestamp}:`);
-  console.log(`transaction target address: ${arguments[3]}`);
+    console.log(`Listening no.#${counter} swap event  at ${timestamp}:`);
+    console.log(`transaction target address: ${arguments[3]}`);
 
-  // Check if argument 6 is an object, if so, stringify it
-  if (typeof arguments[6] === 'object' && arguments[6] !== null) {
-    console.log(`transaction details (argument 6): ${JSON.stringify(arguments[6], null, 2)}`);
-  } else {
-    console.log(`transaction details (argument 6): ${arguments[6]}`);
+    // Check if argument 6 is an object, if so, stringify it
+    if (typeof arguments[6] === 'object' && arguments[6] !== null) {
+      console.log(`transaction details (argument 6): ${JSON.stringify(arguments[6], null, 2)}`);
+    } else {
+      console.log(`transaction details (argument 6): ${arguments[6]}`);
+    }
+  } catch (error) {
+    console.error(`Pair listener: An error occurred while processing the swap event: ${error}`);
   }
 });
 
-
+provider.on('error', (error) => {
+  console.error(`Pair listener: An error occurred with the provider: ${error}`);
+});

185ae5088a053227be841ba045edac4c047adef3
M <129726658+mat1ab@users.noreply.github.com>
2023-07-24 14:02:52 -0700

    test

--- src/basePoolListener.js
+++ src/basePoolListener.js
@@ -1,11 +1,10 @@
 const ethers = require('ethers');
 const AWS = require('aws-sdk');
-const addresses = require('./config/farmConfig.json'); 
 
 const PROJ_ROOT = process.env.PROJ_ROOT;
 const config = require(`${PROJ_ROOT}/src/config/config.json`);
-const basePoolAbi = require(`${PROJ_ROOT}/src/abis/basePool_abi.json`);
-
+const gNftAbi = require(`${PROJ_ROOT}/src/abis/gNft_abi.json`);
+const address = config.gNftAddress;
 const provider = new ethers.providers.WebSocketProvider('wss://testnet.era.zksync.dev/ws');
 
 AWS.config.update({
@@ -13,56 +12,84 @@ AWS.config.update({
 });
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-async function handleTransferEvent(tokenId, to, from, lpTokenAddress, contractAddress) {
+async function handleEvent(tokenId, to, from) {
   console.log('Transfer event detected:');
   console.log(`- Token ID: ${tokenId}`);
   console.log(`- To: ${to}`);
   console.log(`- From: ${from}`);
 
-  const compositeKey = `${contractAddress}-${to}`; 
+  // 删除"from"地址的tokenId（如果存在）
+  if (from !== '0x0000000000000000000000000000000000000000') {
+      const deleteParamsFrom = {
+          TableName: 'gNftTransferEvents',
+          Key: {
+              'userAddress': from,
+          },
+          UpdateExpression: "DELETE tokenIds :tokenId",
+          ExpressionAttributeValues: {
+              ":tokenId": dynamoDB.createSet([tokenId.toString()])
+          },
+          ReturnValues: "UPDATED_NEW"
+      };
 
-  const params = {
-      TableName: 'spNftTransferEvents',
-      Item: {
-        'compositeKey': compositeKey,
-        'tokenId': Number(tokenId),
-        'lpTokenAddress': lpTokenAddress,
-        'userAddress': to,
-        'from': from,
-        'spNftAddress':contractAddress
+      try {
+          await dynamoDB.update(deleteParamsFrom).promise();
+          console.log("Removed tokenId from old userAddress successfully.");
+      } catch (err) {
+          console.error("Unable to delete tokenId from 'from' address. Error JSON:", JSON.stringify(err, null, 2));
       }
+  }
+
+  // 向"to"地址添加tokenId
+  const addParamsTo = {
+      TableName: 'gNftTransferEvents',
+      Key: {
+          'userAddress': to,
+      },
+      UpdateExpression: "ADD tokenIds :tokenId",
+      ExpressionAttributeValues: {
+          ":tokenId": dynamoDB.createSet([tokenId.toString()])
+      },
+      ReturnValues: "UPDATED_NEW"
   };
 
   try {
-      await dynamoDB.put(params).promise();
-      console.log("Added item successfully.");
+      await dynamoDB.update(addParamsTo).promise();
+      console.log("Added tokenId to new userAddress successfully.");
   } catch (err) {
-      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      console.error("Unable to add tokenId to 'to' address. Error JSON:", JSON.stringify(err, null, 2));
   }
 }
 
-async function getPastEvents(pairContract) {
-  const filter = pairContract.filters.Transfer(null, null, null);
-  const events = await pairContract.queryFilter(filter, 0, 'latest');
-  
-  for (const event of events) {
-    const {from, to, tokenId} = event.args;
-    await handleTransferEvent(tokenId, to, from, pairContract.lpTokenAddress, pairContract.address);
-  }
+let latestBlock = 0;  
+
+async function getPastEvents(contract) {
+    let filter = contract.filters.Transfer();
+
+    let events = await contract.queryFilter(filter, latestBlock + 1, 'latest');
+
+    for (let event of events) {
+        const {args} = event;
+        await handleEvent(args.tokenId, args.to, args.from);
+
+        latestBlock = event.blockNumber;
+    }
 }
 
 async function start() {
-  for (let address of addresses) {
-    const pairContract = new ethers.Contract(address.contractAddress, basePoolAbi, provider);
-    pairContract.lpTokenAddress = address.lpTokenAddress;  
-    await getPastEvents(pairContract);
-    pairContract.on("Transfer", async (from, to, tokenId) => { 
-        await handleTransferEvent(tokenId, to, from, pairContract.lpTokenAddress, pairContract.address);
+    const contract = new ethers.Contract(address, gNftAbi, provider);
+    await getPastEvents(contract);
+
+    contract.on("Transfer", async (from, to, tokenId) => {
+        await handleEvent(tokenId, to, from);
     });
-  }
+
+    setInterval(async () => {
+        await getPastEvents(contract);
+    }, 60000);
 }
+
 
 module.exports = {
   start
 };
-

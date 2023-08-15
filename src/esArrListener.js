7b319e68a240795034ef07b736019b64ba128cbe
M <129726658+mat1ab@users.noreply.github.com>
2023-08-15 12:13:28 -0700

    Update esArrListner and fix bug

--- src/esArrListener.js
+++ src/esArrListener.js
@@ -1,185 +1,182 @@
-// const ethers = require('ethers');
-// const AWS = require('aws-sdk');
+const ethers = require('ethers');
+const AWS = require('aws-sdk');
 
-// PROJ_ROOT=process.env.PROJ_ROOT;
-// const provider = new ethers.providers.WebSocketProvider('wss://testnet.era.zksync.dev/ws');
-// const config = require(`${PROJ_ROOT}/src/config/config.json`); 
-// const logger = require(`${PROJ_ROOT}/src/winston`);
-// const esArrTokenAbi = require(`${PROJ_ROOT}/src/abis/esArrToken_abi.json`);
+PROJ_ROOT=process.env.PROJ_ROOT;
+const provider = new ethers.providers.WebSocketProvider('wss://testnet.era.zksync.dev/ws');
+const config = require(`${PROJ_ROOT}/src/config/config.json`); 
+const logger = require(`${PROJ_ROOT}/src/winston`);
+const esArrTokenAbi = require(`${PROJ_ROOT}/src/abis/esArrToken_abi.json`);
 
-// const esArrTokenAddress = config.esArrTokenAddress;
-// AWS.config.update({
-//   region: config.awsRegion,
-// });
+const esArrTokenAddress = config.esArrTokenAddress;
+AWS.config.update({
+  region: config.awsRegion,
+});
 
-// const dynamoDB = new AWS.DynamoDB.DocumentClient();
-// let counter = 0;
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-// async function getTransactionDetails(transactionHash) {
-//   try {
-//     const transaction = await provider.getTransaction(transactionHash);
-//     logger.info(`From:${transaction.from}`);
-//     console.log('From:', transaction.from);
-//     return transaction.from.toString();
-//   } catch (error) {
-//     logger.error(`Error getting transaction details:${error}`);
-//     console.error('Error getting transaction details:', error);
-//     return null;
-//   }
-// }
-
-// async function storeEventToDynamoDB(userID, transactionHash, eventName, blockNumber, eventData, timestamp, esArrTokenAddress) {
-//   const params = {
-//     TableName: 'ADP1',
-//     Item: {
-//       'UserID': userID,
-//       'TransactionHash': transactionHash,
-//       'EventName': eventName,
-//       'BlockNumber': blockNumber,
-//       'EventData': eventData,
-//       'Timestamp': timestamp,
-//       'PairAddress': esArrTokenAddress
-//     }
-//   };
-
-//   try {
-//     await dynamoDB.put(params).promise();
-//     logger.info(`Successfully stored event ${eventName} with transaction hash ${transactionHash}`);
-//     console.log(`Successfully stored event ${eventName} with transaction hash ${transactionHash}`);
-//   } catch (err) {
-//     logger.error(`Error occurred when storing event: ${err}`);
-//     console.error(`Error occurred when storing event: ${err}`);
-//   }
-// }
-
-// async function handleRedeemEvent(sender, amount0, amount1, event) {
-//   counter++;
-//   const timestamp = Math.floor(Date.now() / 1000);
-//   const transactionHash = event.transactionHash;
-//   const userID = await getTransactionDetails(transactionHash);
-//   logger.info(`Listening no.#${counter} Redeem event at ${timestamp}:`);
-//   console.log(`Listening no.#${counter} Redeem event at ${timestamp}:`);
-
-
-//   await storeEventToDynamoDB(
-//     userID,
-//     transactionHash,
-//     'Redeem',
-//     event.blockNumber,
-//     JSON.stringify(event),
-//     timestamp,
-//     event.address
-//   );
-//   logger.info(`Event object: ${JSON.stringify(event, null, 2)}`);
-//   console.log(`Event object: ${JSON.stringify(event, null, 2)}`);
-// }
-
-// async function handleConvertEvent(sender, amount0, amount1, to, event) {
-//   counter++;
-//   const timestamp = Math.floor(Date.now() / 1000);
-//   const transactionHash = event.transactionHash;
-//   const userID = await getTransactionDetails(transactionHash);
-//   logger.info(`Listening no.#${counter} Convert event at ${timestamp}:`);
-//   console.log(`Listening no.#${counter} Convert event at ${timestamp}:`);
-
-//   await storeEventToDynamoDB(
-//     userID,
-//     transactionHash,
-//     'Convert',
-//     event.blockNumber,
-//     JSON.stringify(event),
-//     timestamp,
-//     event.address
-//   );
-//   logger.info(`Event object: ${JSON.stringify(event, null, 2)}`);
-//   console.log(`Event object: ${JSON.stringify(event, null, 2)}`);
-// }
-
-
-// async function handleCancelRedeemEvent(sender, amount0, amount1, to, event) {
-//     counter++;
-//     const timestamp = Math.floor(Date.now() / 1000);
-//     const transactionHash = event.transactionHash;
-//     const userID = await getTransactionDetails(transactionHash);
-//     logger.info(`Listening no.#${counter} CancelRedeem event at ${timestamp}:`);
-//     console.log(`Listening no.#${counter} CancelRedeem event at ${timestamp}:`);
+async function getTransactionDetails(transactionHash) {
+    try {
+      const transaction = await provider.getTransaction(transactionHash);
+      logger.info(`From:${transaction.from}`);
+      console.log('From:', transaction.from);
+      const userFrom = transaction.from.toString();
+      return typeof userFrom === 'string' ? userFrom : 'NULL';
+    } catch (error) {
+      logger.error(`Error getting transaction details:${error}`);
+      console.error('Error getting transaction details:', error);
+      return 'NULL';  // todo: handle userID 
+    }
+  }
   
-//     await storeEventToDynamoDB(
-//       userID,
-//       transactionHash,
-//       'CancelRedeem',
-//       event.blockNumber,
-//       JSON.stringify(event),
-//       timestamp,
-//       event.address
-//     );
-//     logger.info(`Event object: ${JSON.stringify(event, null, 2)}`);
-//     console.log(`Event object: ${JSON.stringify(event, null, 2)}`);
-//   }
 
-// async function handleAllocateEvent(sender, amount0, amount1, to, event) {
-//     counter++;
-//     const timestamp = Math.floor(Date.now() / 1000);
-//     const transactionHash = event.transactionHash;
-//     const userID = await getTransactionDetails(transactionHash);
-//     logger.info(`Listening no.#${counter} Allocate event at ${timestamp}:`);
-//     console.log(`Listening no.#${counter} Allocate event at ${timestamp}:`);
-  
-//     await storeEventToDynamoDB(
-//       userID,
-//       transactionHash,
-//       'Allocate',
-//       event.blockNumber,
-//       JSON.stringify(event),
-//       timestamp,
-//       event.address
-//     );
-//     logger.info(`Event object: ${JSON.stringify(event, null, 2)}`);
-//     console.log(`Event object: ${JSON.stringify(event, null, 2)}`);
-//   }
+async function storeEventToDynamoDB(userID, transactionHash, eventName, blockNumber, eventData, timestamp, esArrTokenAddress) {
+  const params = {
+    TableName: 'ADP1',
+    Item: {
+      'UserID': userID,
+      'TransactionHash': transactionHash,
+      'EventName': eventName,
+      'BlockNumber': blockNumber,
+      'EventData': eventData,
+      'Timestamp': timestamp,
+      'PairAddress': esArrTokenAddress
+    }
+  };
 
-// async function handleDeallocateEvent(sender, amount0, amount1, to, event) {
-//     counter++;
-//     const timestamp = Math.floor(Date.now() / 1000);
-//     const transactionHash = event.transactionHash;
-//     const userID = await getTransactionDetails(transactionHash);
-//     logger.info(`Listening no.#${counter} Deallocate event at ${timestamp}:`);
-//     console.log(`Listening no.#${counter} Deallocate event at ${timestamp}:`);
-  
-//     await storeEventToDynamoDB(
-//       userID,
-//       transactionHash,
-//       'Deallocate',
-//       event.blockNumber,
-//       JSON.stringify(event),
-//       timestamp,
-//       event.address
-//     );
-//     logger.info(`Event object: ${JSON.stringify(event, null, 2)}`);
-//     console.log(`Event object: ${JSON.stringify(event, null, 2)}`);
-//   }
+  try {
+    await dynamoDB.put(params).promise();
+    logger.info(`Successfully stored event ${eventName} with transaction hash ${transactionHash}`);
+    console.log(`Successfully stored event ${eventName} with transaction hash ${transactionHash}`);
+  } catch (err) {
+    logger.error(`Error occurred when storing event: ${err}`);
+    console.error(`Error occurred when storing event: ${err}`);
+  }
+}
 
-// async function start(provider) {
-//     const listenForEsArrEvents = async () => {
-//       const pairAddress = esArrTokenAddress;
-//       const pairContract = new ethers.Contract(pairAddress, esArrTokenAbi, provider);
+async function handleRedeemEvent(sender, amount0, amount1, event) {
+  const timestamp = Math.floor(Date.now() / 1000);
+  const transactionHash = event.transactionHash;
+  const userID = await getTransactionDetails(transactionHash);
 
-//       pairContract.on('Redeem', handleRedeemEvent);
-//       pairContract.on('Convert', handleConvertEvent);
-//       pairContract.on('CancelRedeem', handleCancelRedeemEvent);
-//       pairContract.on('Allocate', handleAllocateEvent);
-//       pairContract.on('Deallocate', handleDeallocateEvent);
 
-//     };
+  await storeEventToDynamoDB(
+    userID,
+    transactionHash,
+    'Redeem',
+    event.blockNumber,
+    JSON.stringify(event),
+    timestamp,
+    event.address
+  );
+  logger.info(`Event object: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Event object: ${JSON.stringify(event, null, 2)}`);
+}
+
+async function handleConvertEvent(from, to, amount, event) {
+    if (!event) {
+      logger.error("Undefined event received in handleConvertEvent.");
+      return;
+    }
   
-//     await listenForEsArrEvents();
+    const timestamp = Math.floor(Date.now() / 1000);
+    const transactionHash = event.transactionHash;
   
-//     setInterval(async () => {
-//       await listenForEsArrEvents();
-//     }, 30000);
-//   }
+    if (!transactionHash) {
+      logger.error("Undefined transactionHash in handleConvertEvent.");
+      return;
+    }
   
-// module.exports = {
-//   start
-// };
+    const userID = await getTransactionDetails(transactionHash);
+  
+    await storeEventToDynamoDB(
+      userID,
+      transactionHash,
+      'Convert',
+      event.blockNumber,
+      JSON.stringify(event),
+      timestamp,
+      event.address
+    );
+  
+    logger.info(`Event object: ${JSON.stringify(event, null, 2)}`);
+  }
+
+
+async function handleCancelRedeemEvent(userAddress, esArrAmount, event) {
+    const timestamp = Math.floor(Date.now() / 1000);
+    const transactionHash = event.transactionHash;
+    const userID = await getTransactionDetails(transactionHash);
+  
+    await storeEventToDynamoDB(
+      userID,
+      transactionHash,
+      'CancelRedeem',
+      event.blockNumber,
+      JSON.stringify(event),
+      timestamp,
+      event.address
+    );
+    logger.info(`Event object: ${JSON.stringify(event, null, 2)}`);
+    console.log(`Event object: ${JSON.stringify(event, null, 2)}`);
+  }
+
+async function handleAllocateEvent(userAddress, usageAddress, amount, event) {
+    const timestamp = Math.floor(Date.now() / 1000);
+    const transactionHash = event.transactionHash;
+    const userID = await getTransactionDetails(transactionHash);
+  
+    await storeEventToDynamoDB(
+      userID,
+      transactionHash,
+      'Allocate',
+      event.blockNumber,
+      JSON.stringify(event),
+      timestamp,
+      event.address
+    );
+    logger.info(`Event object: ${JSON.stringify(event, null, 2)}`);
+    console.log(`Event object: ${JSON.stringify(event, null, 2)}`);
+  }
+
+async function handleDeallocateEvent(userAddress, usageAddress, amount, fee, event) {
+    const timestamp = Math.floor(Date.now() / 1000);
+    const transactionHash = event.transactionHash;
+    const userID = await getTransactionDetails(transactionHash);
+  
+    await storeEventToDynamoDB(
+      userID,
+      transactionHash,
+      'Deallocate',
+      event.blockNumber,
+      JSON.stringify(event),
+      timestamp,
+      event.address
+    );
+    logger.info(`Event object: ${JSON.stringify(event, null, 2)}`);
+    console.log(`Event object: ${JSON.stringify(event, null, 2)}`);
+  }
+
+async function start(provider) {
+    const listenForEsArrEvents = async () => {
+      const pairAddress = esArrTokenAddress;
+      const pairContract = new ethers.Contract(pairAddress, esArrTokenAbi, provider);
+
+      pairContract.on('Redeem', handleRedeemEvent);
+      pairContract.on('Convert', handleConvertEvent);
+      pairContract.on('CancelRedeem', handleCancelRedeemEvent);
+      pairContract.on('Allocate', handleAllocateEvent);
+      pairContract.on('Deallocate', handleDeallocateEvent);
+
+    };
+  
+    await listenForEsArrEvents();
+  
+    setInterval(async () => {
+      await listenForEsArrEvents();
+    }, 30000);
+  }
+  
+module.exports = {
+  start
+};
 
--- src/swapListener.js
+++ src/swapListener.js
@@ -3,9 +3,11 @@ const AWS = require('aws-sdk');
 
 const PROJ_ROOT = process.env.PROJ_ROOT;
 
-const pairAbi = require(`${PROJ_ROOT}/src/abis/pair_abi.json`);
 const logger = require(`${PROJ_ROOT}/src/winston`);
 const config = require(`${PROJ_ROOT}/src/config/config.json`); 
+
+const pairAbi = require(`${PROJ_ROOT}/src/abis/pair_abi.json`);
+const atDexRouterAbi = require(`${PROJ_ROOT}/src/abis/atCoreRouter_abi.json`);
 const atDexRouterAddress = config.atDexRouterAddress;
 
 let registeredPairs = new Set();
@@ -20,10 +22,15 @@ AWS.config.update({
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 
-
 import('chalk').then((module) => {
   chalk = module.default;
 });
+
+async function handleForEthSwapEvent(...args) {
+  const event = args[args.length - 1];
+  console.log(JSON.stringify(event, null, 2));
+}
+
 
 async function loadPairAddressesFromDB() {
   const params = {
@@ -58,7 +65,7 @@ function convertToNormalNumber(inputNumber) {
 
 
 async function storeEventToDynamoDB(userID, transactionHash, eventName, blockNumber, eventData, timestamp, pairAddress) {
-const params = {
+  const params = {
   TableName: 'ADP1', 
   Item: {
     'UserID': userID,
@@ -76,15 +83,14 @@ const params = {
     }
   }
 };
-
-try {
+  try {
   await dynamoDB.put(params).promise();
   logger.info(`Successfully stored event ${eventName} with transaction hash ${transactionHash}`);
   console.log(`Successfully stored event ${eventName} with transaction hash ${transactionHash}`);
   delete swapStorage[transactionHash];
 } catch (err) {
-  logger.error(`Error occurred when storing event: ${err}`);
-  console.error(chalk.red(`Error occurred when storing event: ${err}`));
+    logger.error(`Error occurred when storing event: ${err}`);
+    console.error(chalk.red(`Error occurred when storing event: ${err}`));
 }
 }
 
@@ -94,6 +100,11 @@ async function handleSwapEvent(userID, amount0In, amount1In, amount0Out, amount1
     amount1In = convertToNormalNumber(amount1In);
     amount0Out = convertToNormalNumber(amount0Out);
     amount1Out = convertToNormalNumber(amount1Out);
+    console.log(`************************`);
+    console.log(`amount0In: ${amount0In}`);
+    console.log(`amount1In: ${amount1In}`);
+    console.log(`amount0Out: ${amount0Out}`);
+    console.log(`amount1Out: ${amount1Out}`);
 
     if (event.transactionHash) {
       if (!swapStorage[event.transactionHash]) {
@@ -162,16 +173,17 @@ setInterval(() => {
   // addressSwapStorage = {};
 }, 10 * 60 * 1000);
 
-
 async function start(provider) {
   console.log('xxxxxxxxxxxswap');
+ 
+  const routerContract = new ethers.Contract(atDexRouterAddress, atDexRouterAbi, provider);
+  routerContract.on('ForEthSwap', handleForEthSwapEvent);
+
   const pairAddresses = await loadPairAddressesFromDB();
   pairAddresses.forEach(pairAddress => {
-    if (!registeredPairs.has(pairAddress)) {
       const pairContract = new ethers.Contract(pairAddress, pairAbi, provider);
       pairContract.on('Swap', handleSwapEvent);
-      registeredPairs.add(pairAddress);
-    }
+     
   });
 }
 
